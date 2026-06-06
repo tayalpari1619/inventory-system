4bcc2942e063a1957cfce590e922b05ab9de60f4
unknown <tayal.paritayal@gmail.com>
2026-06-06 16:23:39 +0530

    saving uncommited changes

---
+++ inventory-system/backend/app/models/__init__.py
@@ -0,0 +1,4 @@
+from app.models.product import Product
+from app.models.customer import Customer
+from app.models.order import Order, OrderItem, OrderStatus
+__all__ = ["Product", "Customer", "Order", "OrderItem", "OrderStatus"]
